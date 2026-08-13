91e9ef8510e783f58fab0366ad986151b671e495
Tyler Zacharias <zzsaltandpepperzz@icloud.com>
2026-08-12 21:11:26 -0400

    created useClock hook

--- src/hooks/useClock.ts
+++ src/hooks/useClock.ts
@@ -2,7 +2,11 @@ import { useAppStore } from "@/state/store";
 import { useEffect } from "react";
 
 function useClock() {
-
-
-  
+  const tick = useAppStore.getState().tick
+  useEffect(() => {
+    const interval = setInterval(() => {
+      tick(Date.now())
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
 }
