7f7ddc178ba14f82e959b3ab0369f56173bc1e84
Hannah <wagner.hannah.2006@gmail.com>
2026-08-14 21:24:42 -0400

    Added circular progress indicator pkg

--- src/ui/MainScreen.tsx
+++ src/ui/MainScreen.tsx
@@ -1,10 +1,11 @@
 import { Button, ButtonText } from "@/components/ui/button";
 import { useClock } from "@/src/hooks/useClock";
+import CircularProgress from "react-native-circular-progress-indicator";
 
 export default function MainScreen() {
   useClock();
   return (
-    <Button variant="default" size="default">
+    <Button variant="outline" size="lg">
       <ButtonText>This Is A Button</ButtonText>
     </Button>
   );
