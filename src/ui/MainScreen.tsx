8d1df66fcc44992f3d9f9e27efce960e438d9be6
Hannah <wagner.hannah.2006@gmail.com>
2026-08-14 22:39:18 -0400

    Modified Index, added empty screen files

--- src/ui/MainScreen.tsx
+++ src/ui/MainScreen.tsx
@@ -1,11 +1,14 @@
 import { Button, ButtonText } from "@/components/ui/button";
+import { View, Text } from "react-native";
 import { useClock } from "@/src/hooks/useClock";
 
 export default function MainScreen() {
-  useClock();
   return (
-    <Button variant="outline" size="lg">
-      <ButtonText>BUTT on</ButtonText>
-    </Button>
+    // <Button variant="outline" size="lg" className="flex">
+    //   <ButtonText className="text-9xl">Schedule</ButtonText>
+    // </Button>
+    <View>
+      <Text>Hello</Text>
+    </View>
   );
 }
